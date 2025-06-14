bebbf32ae808dc708b1ac5f1d1f0ec21777962e1
Vanexaa <reuterasvanessa@gmail.com>
2025-06-14 22:05:41 +0800

    updated pet available

--- OOP-PROJECT/Program.cs
+++ OOP-PROJECT/Program.cs
@@ -21,6 +21,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Carousel}/{id?}");
 
 app.Run();  
--- OOP-PROJECT/obj/Debug/net8.0/OOP-PROJECT.AssemblyInfo.cs
+++ OOP-PROJECT/obj/Debug/net8.0/OOP-PROJECT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP-PROJECT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65af2a9939cd8cd482b2f5613e6628c8346c529")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP-PROJECT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP-PROJECT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
